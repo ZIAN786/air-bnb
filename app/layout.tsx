f499118c3a5a3c09253f492c796eb2dafe4d1bf2
ZIAN786 <zakirzian41@gmail.com>
2024-06-06 04:10:58 +0600

    holiday pack app

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/modals/RegisterModal'
+import LoginModal from './components/modals/LoginModal'
+import RentModal from './components/modals/RentModal'
+import SearchModal from './components/modals/SearchModal'
+
+import ToasterProvider from './providers/ToasterProvider'
+import getCurrentUser from './actions/getCurrentUser'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser()
+
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <SearchModal />
+          <RentModal />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className='pb-20 pt-28'> {children} </div>
+      </body>
+    </html>
+  )
+}
